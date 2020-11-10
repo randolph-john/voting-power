d5001f4ffc64c3b048b29ed0cfb46c312bc8e651
John0Randolph <john_randolph@brown.edu>
2020-11-09 19:27:25 -0500

    first commit

---
+++ voting_power_simulation.py
@@ -0,0 +1,18 @@
+# imports
+
+# constants
+ECONOMIST = True # true if economist, false if fivethityeight
+
+# import individual state probabilities
+# import map from state to electoral votes and population
+    # options for reapportionment and for DC, puerto rico statehood
+    # calculate sum of votes, total needed
+
+# first, try with Banzhaf power index
+# initialize empty map state -> num tallies
+# do this a bunch of times
+    # initialize empty list for Red and Blue states
+    # for each state, flip a coin and sort into lists
+    # for each state in winning coalition, tally
+# for each state, divide by population
+# output power for each state and for each voter
